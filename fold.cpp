a8d266bfef9295b843ca2a5f41b76e9a91931a5a
GuyShiboleth <guyshiboleth76@gmail.com>
2025-06-17 17:48:28 +0300

    nicer formatting

--- fold.cpp
+++ fold.cpp
@@ -3,8 +3,8 @@
 #define MAX_MULTIPLIER (11)
 
 int main() {
-	std::cout << "   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10| 11|" << std::endl;
-	std::cout << "   ---------------------------------------------" << std::endl;
+	std::cout << "   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10| 11" << std::endl;
+	std::cout << "   --------------------------------------------" << std::endl;
 	for (size_t i = 1; i <= MAX_MULTIPLIER; i++) {
 		std::cout << i << (i > 9 ? " " : "  ");
 		for (size_t j = 1; j <= MAX_MULTIPLIER; j++) {
