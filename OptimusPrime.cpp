10903b5770f492bb6440ce29bfd4fa819cc57000
GuyShiboleth <guyshiboleth76@gmail.com>
2025-06-18 11:33:43 +0300

    check if a number is prime

--- OptimusPrime.cpp
+++ OptimusPrime.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
 
-bool isPrime(int num) {
+#define VALID_EXIT_CODE (0)
+#define INVALID_EXIT_CODE (1)
+
+using std::sqrt;
+using std::endl;
+using std::cout;
+
+/*
+* Checks if a number is prime
+*
+* :param[IN] num: The number to check if it's
+* :param[OUT] num: A boolean value indicating if the input is prime, if the input is invalid
+*
+* :return: A status code indicating if there was an error with the function
+*/
+int isPrime(int num, bool* primeStatus);
+
+int main() {
+	bool primeStatus = false;
+	int isPrimeStatusCode = VALID_EXIT_CODE;
+	
+	isPrimeStatusCode = isPrime(47, &primeStatus);
+	cout << (primeStatus ? "True" : "False") << endl;
+
+	return isPrimeStatusCode;
+}
+
+int isPrime(int num, bool* primeStatus) {
 	if (num < 2) {
-		return false;
+		return INVALID_EXIT_CODE;
 	}
 
-	int maxPossibleDivider = static_cast<int>(std::sqrt(num));
+	int maxPossibleDivider = static_cast<int>(sqrt(num));
 	for (size_t i = 2; i <= maxPossibleDivider; i++) {
 		if (num % i == 0) {
 			return false;
 		}
 	}
+	return VALID_EXIT_CODE;
 }
 
-
-int main() {
-	std::cout << (isPrime(5) ? "True" : "False") << std::endl;
-}
