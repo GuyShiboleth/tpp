6ad987610b09ce0dfea188949bba54e234b7f93f
GuyShiboleth <guyshiboleth76@gmail.com>
2025-06-18 12:13:48 +0300

    use more c++ capabilities and improve readability

---
+++ Groot.h
@@ -0,0 +1,6 @@
+/**
+* Gets a positive number from the user
+*
+* @return The number the user inputted or 0 if the input was invalid
+*/
+int getPositiveNum();
--- groot.cpp
+++ groot.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Groot.h"
 
 #define VALID_EXIT_CODE (0)
 #define INVALID_INPUT_EXIT_CODE (1)
@@ -7,43 +8,26 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::sqrt;
-
-/**
-* Gets a positive number from the user
-*
-* :return: The number the user inputted or 0 if the input was invalid
-*/
-int getPositiveNum();
-
-/**
-* Gets a number and prints it's square root
-*
-* :return: An exit code determining if there was an error or not
-*/
-int printSquareRoot(int num);
-
-int main() {
-	int num = getPositiveNum();
-	return 0 == num ? INVALID_INPUT_EXIT_CODE : printSquareRoot(num);
-}
+using std::cerr;
 
 int getPositiveNum() {
-	double num = 0;
+	int num = 0;
 
-	cout << "Enter a number to get the square root of: ";
+	cout << "Enter a number to get the square root of:" << endl;
 	cin >> num;
 
-	if (num <= 0) {
+	if (num < 0) {
 		cout << "Invalid input, input needs to a positive number" << endl;
 		return 0;
 	}
 
 	return num;
+
 }
 
-int printSquareRoot(int num) {
-	if (num < 0) {
-		cout << "Can't get the square root of a negative number" << endl;
+int main() {
+	int num = getPositiveNum();
+	if (0 == num) {
 		return INVALID_INPUT_EXIT_CODE;
 	}
 
