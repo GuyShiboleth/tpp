050bd7f047372a2d2b5e59001a4ea6193a94ddf4
GuyShiboleth <guyshiboleth76@gmail.com>
2025-06-19 11:39:57 +0300

    use output formatting

--- fold.cpp
+++ fold.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
+#include <iomanip>
 
 #define MAX_MULTIPLIER (11)
 
+using std::cout;
+using std::endl;
+using std::setw;
+using std::left;
+
+
 int main() {
-	std::cout << "   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10| 11" << std::endl;
-	std::cout << "   --------------------------------------------" << std::endl;
+	cout << "   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |10 |11 " << endl;
+	cout << "   --------------------------------------------" << endl;
+	
 	for (size_t i = 1; i <= MAX_MULTIPLIER; i++) {
-		std::cout << i << (i > 9 ? " " : "  ");
+		cout << setw(3) << left << i;
 		for (size_t j = 1; j <= MAX_MULTIPLIER; j++) {
-			std::cout << "|";
-			int product = i * j;
-			if (product < 10) {
-				std::cout << " " << product << " ";
-			} else if (product < 100) {
-				std::cout << " " << product;
-			} else {
-				std::cout << product;
-			}
+			cout << "|" << setw(3) << i * j;
 		}
-		std::cout << std::endl;
+		cout << endl;
 	}
 }
